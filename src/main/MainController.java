502b5b44f406d35a1907a5b5d89ea68ca681d96c
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-08-28 19:43:58 +0200

    start makeing controll panel

---
+++ src/main/MainController.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.Scanner;
+
+public class MainController {
+	
+	private static Scanner in = new Scanner(System.in);
+	
+	public static void EmployyPanel() {
+		
+		System.out.println("Welcome to MAIN MANU");
+		System.out.println("Choose your option:");
+		System.out.println("1. Rent a book");
+		System.out.println("2. Rent a Music");
+		System.out.println("3. Rent a Film");
+		System.out.println("4. Return item");
+		System.out.println("5. Add User");
+		System.out.println("6. Delete User ");
+		System.out.println("7. Show users ");
+		System.out.println("8. ");
+		System.out.println("9. ");
+		System.out.println("0. Quit");
+		
+		int a = in.nextInt();
+		
+		switch(a) {
+		
+		case 1:
+			
+		}
+		
+		
+	}
+	
+	public static void UserPanel() {
+		
+		System.out.println("Welcome to MAIN MANU");
+		System.out.println("Choose your option:");
+		System.out.println("1. Show your items");
+		System.out.println("2. Search for item");
+		System.out.println("3. ");
+		System.out.println("0. Quit");
+	}
+
+}
