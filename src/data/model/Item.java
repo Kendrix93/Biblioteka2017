c1ab029b96bcfb336c4bcce6376f73e35e64ce08
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-08-28 19:07:00 +0200

    add music

--- src/data/model/Item.java
+++ src/data/model/Item.java
@@ -2,12 +2,12 @@ package data.model;
 
 public abstract class Item {
 
-	String id;
-	String type;
-	String name;
-	String author;
-	String company;
-	int counter;
+	private String id;
+	private String type;
+	private String name;
+	private String author;
+	private String company;
+	private int counter;
 	
 	public Item(String id, String type, String name, String author, String company, int counter){
 		this.id = id;
@@ -17,6 +17,112 @@ public abstract class Item {
 		this.company = company;
 		this.counter = counter;
 		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((company == null) ? 0 : company.hashCode());
+		result = prime * result + counter;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (counter != other.counter)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", type=" + type + ", name=" + name + ", author=" + author + ", company=" + company
+				+ ", counter=" + counter + "]";
 	}
 	
 	
---
+++ src/data/model/Music.java
@@ -0,0 +1,53 @@
+package data.model;
+
+public class Music extends Item{
+	
+	private int songsQuantity;
+
+	public Music(String id, String type, String name, String author, 
+			String company, int songsQuantity, int counter) {
+		super(id, type, name, author, company, counter);
+		// TODO Auto-generated constructor stub
+		
+		this.songsQuantity = songsQuantity;
+		
+	}
+
+	public int getSongsQuantity() {
+		return songsQuantity;
+	}
+
+	public void setSongsQuantity(int songsQuantity) {
+		this.songsQuantity = songsQuantity;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + songsQuantity;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Music other = (Music) obj;
+		if (songsQuantity != other.songsQuantity)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CD [songsQuantity=" + songsQuantity + "]";
+	}
+	
+	
+
+}
