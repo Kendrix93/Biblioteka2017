bb91f68b8c5db46708db7585e85ff9e432845612
DESKTOP-RVHLOVG\patry <patvick93@gmail.com>
2017-08-29 21:02:43 +0200

    reapair add iand show users, now it should working

--- src/utlis/UserManage.java
+++ src/utlis/UserManage.java
@@ -15,11 +15,10 @@ public class UserManage {
 	private static int number = 0;
 	
 	public static void adduser() {
-		
-	
+
 		String name, surname, login, password, userType = "non";
 		int i;
-		
+
 		System.out.println("Name");
 		name = in.next();
 		System.out.println("Surname");
@@ -28,32 +27,40 @@ public class UserManage {
 		login = in.next();
 		System.out.println("Password");
 		password = in.next();
-		
-		for (int a = 0 ; a == 1;) {
 		System.out.println("Type of user");
 		System.out.println("1.User");
 		System.out.println("2.Admin");
-		i = in.nextInt();
-		if (i == 1) {
-			userType = "USER";
-			a++;
-		}else if (i ==2) {
-			userType = "ADMIN";
-			a++;
-		}else {
-			System.out.println("Wrong number!!!");
+		for (int a = 0; a <= 0;) {
+			i = in.nextInt();
+			if (i == 1) {
+				userType = "USER";
+				a++;
+			} else if (i == 2) {
+				userType = "ADMIN";
+				a++;
+			} else {
+				System.out.println("Wrong number!!!");
+			}
 		}
-		}
-		
+
 		number = number + 1;
+
+		users.put(number, new User(name, surname, login, password, userType));
 		
-		
-		users.put(number , new User(name, surname, login, password, userType));
-		
+		System.out.println("DONE");
+
 	}
 	
 	public static void showUsers() {
-			System.out.println(users);
+		
+		for (int i = 1;; i++) {
+			if (users.get(i) == null) {
+				break;
+			}
+				System.out.println(users.get(i));
+			
+		}
+
 		
 		
 	}
